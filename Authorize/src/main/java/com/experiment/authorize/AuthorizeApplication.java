0a18464eb426522e9605ed35781f205f69aae431
crymnc <can.sahintas2324@gmail.com>
2021-07-14 21:48:11 +0300

    Project path change for micro service architecture

--- Authorize/src/main/java/com/experiment/authorize/AuthorizeApplication.java
+++ Authorize/src/main/java/com/experiment/authorize/AuthorizeApplication.java
@@ -1,11 +1,15 @@
 package com.experiment.authorize;
 
+import com.experiment.authorize.util.Timer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthorizeApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
         SpringApplication.run(AuthorizeApplication.class, args);
+        Timer timer =new Timer();
+        timer.log();
     }
 }
